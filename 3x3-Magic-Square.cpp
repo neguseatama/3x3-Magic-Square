ea7e8dfeffffe88f9758e31d02660e9b1e829fd2
Iwasaki Kai <62793333+neguseatama@users.noreply.github.com>
2021-09-13 06:55:53 +0900

    Add files via upload

---
+++ 3x3-Magic-Square.cpp
@@ -0,0 +1,48 @@
+/*
+3x3 Magic Square in C++
+C++による3×3魔方陣（三方陣）の解法
+https://neguse-atama.hatenablog.com
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+    int ans[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            ans[i][j]=0;
+        }
+    }
+    ans[1][1]=5;
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            if(ans[i+1][j+1]==5){
+                ans[i][j]=8;
+                ans[i+2][j+2]=2;
+            }else if(ans[i+1][j-1]==5){
+                ans[i][j]=6;
+                ans[i+2][j-2]=4;
+            }
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(ans[i][j]==0){
+                if(j==0){
+                    ans[i][j]=15-ans[i-1][j]-ans[i+1][j];
+                    ans[i][j+2]=10-ans[i][j];
+                }
+                if(i==0){
+                    ans[i][j]=15-ans[i][j-1]-ans[i][j+1];
+                    ans[i+2][j]=10-ans[i][j];
+                }
+            }
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<ans[i][j];
+            cout<<((j==2) ? "\n" : " ");
+        }
+    }
+    return 0;
+}
